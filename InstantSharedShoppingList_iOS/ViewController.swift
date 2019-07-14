99160a41ec29bcae7c5f5bff3bfeb6c45a807e26
Pier <papeschi@gmail.com>
2019-07-14 11:44:37 +0200

    Initial Commit

---
+++ InstantSharedShoppingList_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstantSharedShoppingList_iOS
+//
+//  Created by Pier Luigi Papeschi on 14/07/2019.
+//  Copyright © 2019 Pier Luigi Papeschi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
